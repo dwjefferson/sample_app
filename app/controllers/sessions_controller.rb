7940728babd1b2a2f558a23eca561077b59b16c1
Dean Jefferson <djefferson@madisoncollege.edu>
2013-11-04 20:24:29 -0600

    Finish sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    userx = User.find_by(email: params[:session][:email].downcase)
+    if userx && userx.authenticate(params[:session][:password])
+      sign_in userx
+      redirect_to userx
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render "new"
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_url
+  end
+end
